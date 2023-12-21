f855d376b08f4745501c34532f96ea4e7ff43451
Johan Commelin <johan@commelin.net>
2023-12-21 22:24:33 +0100

    wip

--- sembreak.py
+++ sembreak.py
@@ -1,31 +1,82 @@
-# Reformat a file according to semantic linebreaking.
-# 
-# Usage: echo <text> | sd sembreak
-
 import argparse
-import nltk
+import pysbd
+import re
 import sys
 
-def semantic_break(text, max_line_length=80):
-    sentences = nltk.sent_tokenize(text)
-    for sentence in sentences:
-        words = sentence.split()
-        line = ''
-        for word in words:
-            if len(line) + len(word) + 1 > max_line_length:
-                if ',' in line:
-                    # Find the last comma in the line
-                    last_comma_index = line.rindex(',')
-                    # Yield the part of the line before the last comma
-                    yield line[:last_comma_index+1]
-                    # Start a new line with the part of the line after the last comma
-                    line = line[last_comma_index+1:].lstrip() + ' ' + word
-                else:
-                    yield line
-                    line = word
-            else:
-                line = line + ' ' + word if line else word
-        yield line
+# Reformat a file according to semantic linebreaking.
+
+# Recursively find the optimal break points within a range of indices (excluding `end`)
+# The cost function `cost_fun(i, j)` should return the cost of the segment `[i, ..., j-1]`
+# The maximum cost `max_cost` should be a large value,
+# larger than the cost of the sum of `cost_fun(i, i+1)` for all `i`
+#
+# Returns a list of indices of the optimal break points,
+# where an index `i` indicates a break point `..., i-1], [i, ...`
+def find_breaks(start, end, cost_fun, max_cost):
+    # Initialize memoization dictionary
+    memo = {}
+    def aux(m, n, level=1):
+        # Base case: no break points
+        if m == n:
+            return (0, [])
+        # Check if the solution is already memoized
+        if (m, n) in memo:
+            return memo[(m, n)]
+        # Initialize the minimum cost array
+        min_cost = max_cost
+        best_breaks = list(range(m, n))
+        # Iterate through all possible break points
+        for i in range(m, n):
+            (cost, breaks) = aux(m, i, level+1)
+            cost += cost_fun(i, n)
+            if cost < min_cost:
+                min_cost = cost
+                best_breaks = breaks + [i]
+        memo[(m, n)] = (min_cost, best_breaks)
+        return (min_cost, best_breaks)
+    return aux(start, end)[1]
+
+def semantic_break(sentence, max_line_length=80):
+    if len(sentence) <= max_line_length:
+        yield sentence
+        return
+    
+    # Todo: ideally we would like to prioritize certain delimiters over others
+
+    # Define delimiters that may be preceded (resp. followed) by a newline character
+    break_before_delimiters = ' \('
+    break_after_delimiters = ',|;|:|\band\b|\bor\b|\) '
+
+    # Replace each delimiter with the same delimiter preceded (resp. followed) by a newline character
+    sentence = re.sub(break_before_delimiters, r'\n\g<0>', sentence)
+    sentence = re.sub(break_after_delimiters, r'\g<0>\n', sentence)
+
+    # Split the sentence into pieces at each newline character
+    pieces = [piece.strip() for piece in sentence.split('\n')]
+
+    # Precompute the lengths of the pieces and the number of pieces
+    lengths = [len(piece) for piece in pieces]
+    N = len(lengths)
+
+    # Define the cost function
+    def cost(i, j):
+        length = sum(lengths[i:j]) + (j - i - 1)
+        if length > max_line_length:
+            return length**2
+        else:
+            return (max_line_length - length)**2
+
+    # Compute the maximum possible cost
+    max_cost = sum(l**2 for l in lengths)
+
+    # Find the optimal break points
+    breaks = find_breaks(0, N, cost, max_cost)
+
+    # Yield the lines
+    k = len(breaks)
+    breaks.append(N)
+    for i in range(k):
+        yield ' '.join(pieces[breaks[i]:breaks[i+1]])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -33,7 +84,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     text = sys.stdin.read()
-    for line in semantic_break(text, args.max_line_length):
-        print(line)
 
-# TODO: add optional argument for line length (default 80)
+    # Bug(pysbd):
+    # a parenthetical sentence ends up being split between the period and the closing parenthesis
+    seg = pysbd.Segmenter(language="en", clean=False)
+    sentences = seg.segment(text)
+    
+    for sentence in sentences:
+        for line in semantic_break(sentence, args.max_line_length):
+            print(line)
